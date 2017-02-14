0b2dbb2038c5548c3fde4f405ad0958c1e83f8ae
Daniel Morais <daniel.moraise@playnext-lab.co.jp>
2017-02-14 13:10:15 +0900

    Paint differences in red background using google DiffMatchPatch

--- app/src/main/java/com/jp/playnext/voicecards/model/Card.java
+++ app/src/main/java/com/jp/playnext/voicecards/model/Card.java
@@ -16,103 +16,90 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static org.bitbucket.cowwoc.diffmatchpatch.DiffMatchPatch.*;
+
 /**
  * Created by danielmorais on 2/8/17.
  */
 
 public class Card {
 
+    private static final String TAG = Card.class.getSimpleName();
     private String sentence;
 
-    public Card(String sentence) {
-        this.sentence = sentence;
-    }
-
-    public SpannableString difference(String text) {
-        return difference(text, null);
-    }
-
-    public SpannableString difference(String text, Color color) {
-        Log.v("CARD", "Expected words:" + sentence);
-        Log.v("CARD", "Words received words:" + text);
-
-        //return differenceDiffLib(text, color);
-        SpannableString spannable = differenceUtils(text, color);
-
-        return spannable;
-    }
-
-    /**
-     * TEST USING GOOGLE LIBRARY
-     * @param text
-     * @param color
-     * @return
-     */
-    private SpannableString differenceDiffLib(String text, Color color) {
-        //TODO Calculate difference betweeen sentence and text
-        //TODO return difference with highlight if color not null
-        List<String> different_words = Utils.difference(sentence, text);
-
-
-        DiffMatchPatch diff = new DiffMatchPatch();
-
-        LinkedList<DiffMatchPatch.Diff> diffs = diff.diffMain(text, sentence);
-
-        diff.diffCleanupEfficiency(diffs);
-
-        Log.v("CARD", "=========DIFF =======");
-
-        for (DiffMatchPatch.Diff diff1 : diffs) {
-            Log.v("CARD", "DIFF:" + diff1.toString());
-        }
-
-        SpannableString spannable = new SpannableString(text);
-
-        String prettyDif = diff.diffPrettyHtml(diffs);
-        Log.v("CARD", "PRETTY DIF: " + prettyDif);
-        Log.v("CARD", "=========END DIFF =======");
-
-        spannable = new SpannableString(Html.fromHtml(prettyDif));
-        return spannable;
-    }
-
-    /**
-     * TEST USING SIMPLE LOCAL LIBRARY
-     * @param text
-     * @param color
-     * @return
-     */
-    private SpannableString differenceUtils(String text, Color color) {
-        //TODO Calculate difference betweeen sentence and text
-        //TODO return difference with highlight if color not null
-        List<String> different_words = Utils.difference(sentence, text);
-        SpannableString spannable = new SpannableString(text);
-
-
-        for (String s : different_words) {
-            Log.v("CARD", "Different words:" + s);
-
-            Pattern pattern = Pattern.compile("(?i)" + s);
-            Matcher matcher = pattern.matcher(text);
-            while (matcher.find()) {
-                spannable.setSpan(
-                        new BackgroundColorSpan(Color.parseColor("#BFFFC6")),
-                        matcher.start(),
-                        matcher.end(),
-                        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-            }
-
-
-        }
-
-        return spannable;
-    }
 
     public String getSentence() {
         return sentence;
     }
 
-    public void setSentence(String sentence) {
+
+    public Card(String sentence) {
         this.sentence = sentence;
     }
+
+    
+    /**
+     *
+     * @param text
+     * @return SpannableString highlighting differences
+     */
+    public SpannableString difference(String text) {
+        return difference(text, null);
+    }
+
+
+    /**
+     *
+     * @param text
+     * @return SpannableString highlighting differences
+     */
+    public SpannableString difference(String text, Color color) {
+        Log.v(TAG, "Expected words:" + sentence);
+        Log.v(TAG, "Words received words:" + text);
+
+        return differenceDiffLib(text, color);
+    }
+
+    /**
+     *
+     * @param text
+     * @param color
+     * @return SpannableString highlighting differences
+     */
+    private SpannableString differenceDiffLib(String text, Color color) {
+        DiffMatchPatch diff = new DiffMatchPatch();
+
+        LinkedList<DiffMatchPatch.Diff> diffs = diff.diffMain(text.toLowerCase(), sentence.toLowerCase());
+        diff.diffCleanupEfficiency(diffs);
+
+        SpannableString spannable = new SpannableString(text);
+        int start = 0;
+
+        for (Diff diff1 : diffs) {
+            int end = start + diff1.text.length();
+
+            switch (diff1.operation) {
+                case DELETE: //Paint red
+                    spannable.setSpan(
+                            new BackgroundColorSpan(Color.parseColor("#FF4081")),
+                            start,
+                            end,
+                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                    break;
+
+                case INSERT: // Don't add difference, jump to next diff
+                    continue;
+
+                case EQUAL://Do nothing
+                    break;
+
+                default:
+                    break;
+            }
+            start = end;
+        }
+
+        return spannable;
+    }
+
 }
