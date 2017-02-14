0c5a9b36f1ad3cee78cad30b5debc7fbfd39201b
Daniel Morais <daniel.moraise@playnext-lab.co.jp>
2017-02-14 16:42:56 +0900

    Improved highlighting . Made card parceable

--- app/src/main/java/com/jp/playnext/voicecards/model/Card.java
+++ app/src/main/java/com/jp/playnext/voicecards/model/Card.java
@@ -1,20 +1,17 @@
 package com.jp.playnext.voicecards.model;
 
 import android.graphics.Color;
-import android.text.Html;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.BackgroundColorSpan;
 import android.util.Log;
 
-import com.jp.playnext.voicecards.Utils;
 
 import org.bitbucket.cowwoc.diffmatchpatch.DiffMatchPatch;
 
 import java.util.LinkedList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import static org.bitbucket.cowwoc.diffmatchpatch.DiffMatchPatch.*;
 
@@ -22,7 +19,7 @@ import static org.bitbucket.cowwoc.diffmatchpatch.DiffMatchPatch.*;
  * Created by danielmorais on 2/8/17.
  */
 
-public class Card {
+public class Card implements Parcelable {
 
     private static final String TAG = Card.class.getSimpleName();
     private String sentence;
@@ -37,9 +34,8 @@ public class Card {
         this.sentence = sentence;
     }
 
-    
+
     /**
-     *
      * @param text
      * @return SpannableString highlighting differences
      */
@@ -49,7 +45,6 @@ public class Card {
 
 
     /**
-     *
      * @param text
      * @return SpannableString highlighting differences
      */
@@ -61,7 +56,6 @@ public class Card {
     }
 
     /**
-     *
      * @param text
      * @param color
      * @return SpannableString highlighting differences
@@ -69,26 +63,45 @@ public class Card {
     private SpannableString differenceDiffLib(String text, Color color) {
         DiffMatchPatch diff = new DiffMatchPatch();
 
-        LinkedList<DiffMatchPatch.Diff> diffs = diff.diffMain(text.toLowerCase(), sentence.toLowerCase());
+        LinkedList<DiffMatchPatch.Diff> diffs = diff.diffMain(sentence.toLowerCase(), text.toLowerCase());
         diff.diffCleanupEfficiency(diffs);
 
-        SpannableString spannable = new SpannableString(text);
+        String newText = "";
+        for (Diff diff1 : diffs) {
+            if (diff1.operation != Operation.INSERT) //Comment to insert different text
+                newText += diff1.text;
+        }
+
+
+        SpannableString spannable = new SpannableString(newText);
         int start = 0;
 
         for (Diff diff1 : diffs) {
             int end = start + diff1.text.length();
 
+            String backgroundColor = "#00000000";
             switch (diff1.operation) {
-                case DELETE: //Paint red
+                case INSERT: //Do nothing
+                    /*
+                    // Uncomment to paint inserted text
+                    backgroundColor = "#FF0000";
                     spannable.setSpan(
-                            new BackgroundColorSpan(Color.parseColor("#FF4081")),
+                            new BackgroundColorSpan(Color.parseColor(backgroundColor)),
                             start,
                             end,
                             Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
                     break;
-
-                case INSERT: // Don't add difference, jump to next diff
+                    */
                     continue;
+
+                case DELETE: //Paint red
+                    backgroundColor = "#FF0000";
+                    spannable.setSpan(
+                            new BackgroundColorSpan(Color.parseColor(backgroundColor)),
+                            start,
+                            end,
+                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                    break;
 
                 case EQUAL://Do nothing
                     break;
@@ -96,10 +109,37 @@ public class Card {
                 default:
                     break;
             }
+
+
             start = end;
         }
 
         return spannable;
     }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.sentence);
+    }
+
+    protected Card(Parcel in) {
+        this.sentence = in.readString();
+    }
+
+    public static final Parcelable.Creator<Card> CREATOR = new Parcelable.Creator<Card>() {
+        @Override
+        public Card createFromParcel(Parcel source) {
+            return new Card(source);
+        }
+
+        @Override
+        public Card[] newArray(int size) {
+            return new Card[size];
+        }
+    };
 }
