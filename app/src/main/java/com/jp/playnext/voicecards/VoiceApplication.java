085bf99a5d97c15f8af51087df904d05ca29f285
Daniel Morais <daniel.moraise@playnext-lab.co.jp>
2017-02-22 17:06:56 +0900

    Added static SharedPrefs to Application

--- app/src/main/java/com/jp/playnext/voicecards/VoiceApplication.java
+++ app/src/main/java/com/jp/playnext/voicecards/VoiceApplication.java
@@ -5,19 +5,35 @@ package com.jp.playnext.voicecards;
  */
 
 import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
 
 import com.jp.playnext.voicecards.database.DBHelper;
 import com.jp.playnext.voicecards.database.DummyInsertData;
 
 public class VoiceApplication extends Application {
 
+    private static final String TAG = VoiceApplication.class.getSimpleName();
+    public static SharedPreferences prefs;
+
+    private final static String DUMMY_DATA_INSERT = "dummy_data_insert";
+
     @Override
     public void onCreate() {
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
         //INIT DB with application context
         DBHelper.getInstance(getApplicationContext());
 
 
-        //DummyInsertData.insertDummyData(this);
+        if (!prefs.getBoolean(DUMMY_DATA_INSERT, false)) {
+            DummyInsertData.insertDummyData(this);
+            prefs.edit().putBoolean(DUMMY_DATA_INSERT, true).commit();
+            Log.v(TAG, "=============Dummy Data added============");
+        }
+
 
         DummyInsertData.getAllCards(this);
         DummyInsertData.getDeckCards(this);
